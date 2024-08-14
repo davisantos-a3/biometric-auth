ad5f52aaad3c6e76f1d8bfc2c5ce163c7a9519fa
Davi Santos <davi.santos@a3apps.com.br>
2024-08-14 15:17:47 -0300

    handle biometric software

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const handleBiometric = async () => {
+    await axios.get("http://localhost:5000/apiservice/identification");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleBiometric()}>BIOMETRIA</button>
     </div>
   );
 }
